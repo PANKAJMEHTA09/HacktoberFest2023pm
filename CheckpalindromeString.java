e436958290501ad29af9a53cca9aec9e339ed60b
PANKAJMEHTA09 <115713511+PANKAJMEHTA09@users.noreply.github.com>
2023-10-22 06:45:03 +0530

    Create CheckpalindromeString.java
    this will help you in checking the palindrome string

---
+++ CheckpalindromeString.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class checkpalindrome {
+    public static boolean IsPalindrome(String st) {
+        int n = st.length();
+        char arr[] = new char[n];
+        for (int i = 0; i < n; i++) {
+            if (st.charAt(i) >= 'a' && st.charAt(i) <= 'z') {
+                arr[i] = (char) (st.charAt(i) - 32);
+            } else {
+                arr[i] = st.charAt(i);
+            }
+        }
+        int sp = 0;
+        int ep = n - 1;
+        while (sp < ep) {
+            if (arr[sp] != arr[ep]) {
+                return false;
+            } else {
+                sp++;
+                ep--;
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(IsPalindrome(st));
+
+    }
+}
+
+
